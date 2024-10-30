d168aff1fb211e8e75e0d43a43eea21e70dc2dd5
Vashisht Boodhun <vb478@live.mdx.ac.uk>
2024-10-30 22:27:11 +0400

    adding images

--- Ai/program.py
+++ Ai/program.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense # type: ignore
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
 import os
 
 # Constants
